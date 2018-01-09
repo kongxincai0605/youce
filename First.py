2e30479e3dfe03c7cdcb82fed96e4adf4707fb85
327333569@qq.com <kongxincai0605>
2018-01-09 09:33:47 +0800

    first

---
+++ First.py
@@ -0,0 +1,29 @@
+#! -*- coding:utf-8 -*-
+
+#__author__ == "kongxincai"
+
+class MyFirst():
+    def __init__(self):
+        pass
+
+    def add(a, b):
+        return a + b
+
+    def sub(a, b):
+        return a - b
+
+    def multiply(a, b):
+        return a * b
+
+    def divide(a, b):
+        return  a / b
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+print("a+b的值" ,a + b)
+print("a-b的值" ,a - b)
+print("a*b的值" ,a * b)
+print("a/b的值" ,a / b)
+
